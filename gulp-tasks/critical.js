98db16ae1d61b87a1e181e0aa668de7916f68a20
Юрий Шаповалов <mr.Shapovaloff@gmail.com>
2020-12-03 13:12:55 +0300

    fix

---
+++ gulp-tasks/critical.js
@@ -0,0 +1,9 @@
+// const critical = require('critical');
+
+// critical.generate({
+//   base: 'test/',
+//   src: 'index.html',
+//   target: 'styles/main.css',
+//   width: 1300,
+//   height: 900,
+// });
